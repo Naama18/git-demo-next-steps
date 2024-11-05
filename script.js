f1b660b460ed30217777006256e86a0a4aeb2424
Naama18 <naamabiton@hilma.tech>
2024-11-05 16:01:53 +0200

    adding console log and my name

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function fooBar() {
-	let counter = -1;
+  let counter = -1;
 
-	while (counter < 10) {
-		console.log("count is " + counter);
-		counter += 2;
-	}
+  while (counter < 10) {
+    console.log("count is " + counter);
+    counter += 2;
+  }
 }
 
 fooBar();
 
 if (new Date().getMonth() === 20) {
-	localStorage.removeItem("user");
+  localStorage.removeItem("user");
+  console.log("deleted");
 }
 
 document.getElementsByTagName("button")[0].addEventListener("click", () => {
-	alert("hi");
+  alert("hi");
 });
